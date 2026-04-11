e926ccca517cdbbbc0c6d21313054047ef4aca6e
Moss??! <tinnaphop4991@gmail.com>
2026-04-11 23:22:43 +0700

    feat: Update Astro configuration to use Vercel adapter and add dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 import { fileURLToPath } from 'node:url';
 
 // https://astro.build/config
 export default defineConfig({
 	output: 'server',
-	adapter: node({ mode: 'standalone' }),
+	adapter: vercel(),
 	integrations: [react()],
 	vite: {
 		plugins: [tailwindcss()],
